41f5797d3fe840a04587d7593a6903b7a69facbf
alcor <155024809+a-lambda@users.noreply.github.com>
2025-01-20 21:52:07 +0100

    fixed schema request

--- SQL/pi_sql_requests.sql
+++ SQL/pi_sql_requests.sql
@@ -1,7 +1,7 @@
 \c alcor
 select version();
-select srid from spatial_ref_sys order by srid desc limit 20;
+select srid from public.spatial_ref_sys order by srid desc limit 20;
 \c exercises
-select * from cd.bookings;
+select * from cd.bookings limit 20;
 \c alcor
 select * from dev.paquets;
