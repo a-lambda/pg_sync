cb0838d9b98f4d6c3ed112751fbe4c862be9c61f
alcor <155024809+a-lambda@users.noreply.github.com>
2025-01-21 22:39:18 +0100

    add some request on new table

--- SQL/pi_sql_requests.sql
+++ SQL/pi_sql_requests.sql
@@ -1,7 +1,18 @@
 \c alcor
 select version();
 select srid from public.spatial_ref_sys order by srid desc limit 20;
+
 \c exercises
 select * from cd.bookings limit 20;
+
 \c alcor
 select * from dev.paquets;
+
+prepare foo as
+select date,shares,trades,dollars
+from dev.factbook
+where date >= $1::date
+and date < $1::date + interval '1 month'
+order by date;
+
+execute foo('2010-02-01');
